08353953e00250a5103e61822a27d0209f124784
Sebastian Deseke <deseke97@gmail.com>
2022-12-07 15:57:22 +0100

    Started with Gui/Render

--- GUI/Renderer.cs
+++ GUI/Renderer.cs
@@ -1,6 +1,54 @@
 namespace CsharpSandbox.GUI;
 
-public class Renders {
+public class Renderer
+{
 
-    
+    public void StoyanExample()
+    {
+        Console.Clear();
+
+        var world = new char[][]
+{
+    new char[] { '+', '-', '-', '-', '+' },
+    new char[] { '|', '#', ' ', ' ', '|' },
+    new char[] { '|', ' ', ' ', ' ', '|' },
+    new char[] { '+', ' ', '-', '-', '+' },
+};
+
+        for (int x = 0; x < world.Length; x++)
+        {
+            for (int y = 0; y < world[x].Length; y++)
+            {
+                Console.Write(world[x][y]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void GenerateField()
+    {
+
+        var Field = new char[][] {
+new char [] { '+', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '+'},
+new char [] { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+new char [] { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+new char [] { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+new char [] { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+new char [] { '+', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '+'},
+};
+
+        for (int x = 0; x < Field.Length; x++)
+        {
+            for (int y = 0; y < Field[x].Length; y++)
+            {
+                Console.Write(Field[x][y]);
+            }
+
+            Console.WriteLine();
+
+        };
+
+
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,13 @@ public class Sandbox
     static void Main(String[] args)
     {
         Battlefield myBattlefield = new Battlefield();
+        var newRender = new GUI.Renderer ();
         Player Player1 = new Player();
         Player Player2 = new Player();
         
         Console.WriteLine("The battlefield is " + myBattlefield.FieldGenerator());
         Console.WriteLine("Spawn at: " + "\n" + myBattlefield.Spawnpoint());
-        myBattlefield.Fight();
-        
+        //myBattlefield.Fight();
+        newRender.GenerateField();
     }
 }
